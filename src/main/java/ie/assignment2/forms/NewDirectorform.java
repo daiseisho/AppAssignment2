8cc3bb0fea904a5d49d05e4e1fae220070ade25e
Brohac <nerovampyro@gmail.com>
2020-11-24 16:56:59 +0000

    Added limitation of data for user inputs for both directorform and movieform

--- src/main/java/ie/assignment2/forms/NewDirectorform.java
+++ src/main/java/ie/assignment2/forms/NewDirectorform.java
@@ -1,5 +1,15 @@
 package ie.assignment2.forms;
 
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Data
 public class NewDirectorform {
+	@Size(min=4, max=30, message="Please make sure that the first name of the director is between 4 and 30 characters")
+	private String firstName;
+	
+	@Size(min=4, max=30, message="Please make sure that the surname of the director is between 4 and 30 characters")
+	private String lastName;
 
 }
--- src/main/java/ie/assignment2/forms/NewMovieForm.java
+++ src/main/java/ie/assignment2/forms/NewMovieForm.java
@@ -1,5 +1,16 @@
 package ie.assignment2.forms;
 
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Min;
+import lombok.Data;
+
+@Data
 public class NewMovieForm {
 
+	@Size(min=4, max=30, message="Please make sure that the word length is between 4 and 30")
+	private String title;
+	
+	@Min(value=1988, message="No movies before 1988")
+	private int year;
+	
 }
