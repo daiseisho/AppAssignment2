6effcacec0fd309575683b8866b10be8a04b0286
Razakai <sean.a.holland@mycit.ie>
2020-11-24 08:25:18 +0000

    removed testing code and some minor changes
    changes include added flex box to pages so items are centered, deleted index controller along with index.html

--- src/main/java/ie/assignment2/controllers/AnyUserControllers.java
+++ src/main/java/ie/assignment2/controllers/AnyUserControllers.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import ie.assignment2.entities.Director;
 import ie.assignment2.entities.Movie;
@@ -69,6 +70,11 @@ public class AnyUserControllers {
 			model.addAttribute("error", "error ocurred when getting all movies");
 			return "notfounderror";
 		}
+	}
+	
+	@GetMapping("/")
+	public String loadHomePage() {
+		return "home";
 	}
 	
 }
