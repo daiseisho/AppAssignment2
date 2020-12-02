8bb45e9d50c9aa2df64ef6576fce063d9f5a01fe
Brohac <nerovampyro@gmail.com>
2020-12-02 19:48:33 +0000

    added basic security with two users
    see security config

---
+++ src/main/java/ie/assignment2/security/WebSecurityConfig.java
@@ -0,0 +1,44 @@
+package ie.assignment2.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	protected UserDetailsService userDetailsDervice() {
+		
+		String encodedPassword = passwordEncoder().encode("password");
+		
+		String adminpassword=passwordEncoder().encode("admin12345");
+		
+		UserDetails user1= User
+				.withUsername("user")
+				.password(encodedPassword)
+				.roles("USER")
+				.build();
+		
+		UserDetails user2=User
+				.withUsername("admin")
+				.password(adminpassword)
+				.roles("ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user1,user2);
+	}
+
+
+}
