2b335e319012b4fd24ff099119a00e7039a14eb0
Brohac <nerovampyro@gmail.com>
2020-12-02 20:49:48 +0000

    Updated websecurity config to differentiate between users

--- src/main/java/ie/assignment2/security/WebSecurityConfig.java
+++ src/main/java/ie/assignment2/security/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package ie.assignment2.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,6 +17,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/css/**","/").permitAll()
+		.antMatchers("/directors").hasAnyRole("ADMIN","USER")
+		.antMatchers("/movies").hasRole("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+		.and()
+		.httpBasic();
+		
+		
+		
+		
 	}
 	
 	@Bean
